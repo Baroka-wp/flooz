cbc8bc1440472bd0ba2717412a12f880d9482b74
baroka <birotori@gmail.com>
2022-09-05 12:26:36 +0100

    feat :implement authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :users, only: [:show] do
+    resources :groups do
+      resources :entities 
+    end
+  end
 end
